c58de248a077fcac8b5cb9e89ec9c03ef0423ab6
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2025-06-14 18:53:51 +0700

    fixed bugs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
 <script setup>
+import { ref } from 'vue'
 import TransactionChecker from './components/TransactionChecker.vue'
+
+const username = ref('')
+const password = ref('')
+
+function onSubmit() {
+  // Add your login logic here
+  // For now, just log the values
+  console.log('Username:', username.value)
+  console.log('Password:', password.value)
+}
 </script>
 
 <template>
-  <TransactionChecker />
+  <div>
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="username"
+        label="Username"
+        placeholder="Username"
+        :rules="[{ required: true, message: 'Username is required' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="password"
+        label="Password"
+        placeholder="Password"
+        :rules="[{ required: true, message: 'Password is required' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="primary" native-type="submit">
+          Submit
+        </van-button>
+      </div>
+    </van-form>
+  </div>
 </template>
+
+<!-- No custom CSS -->
--- src/components/TransactionChecker.vue
+++ src/components/TransactionChecker.vue
@@ -1,36 +1,7 @@
 <template>
   <div>
     <van-nav-bar title="💸 Transaction Checker" />
-
-    <div>
-        <van-form @submit="onSubmit">
-  <van-cell-group inset>
-    <van-field
-      v-model="username"
-      name="username"
-      label="Username"
-      placeholder="Username"
-      :rules="[{ required: true, message: 'Username is required' }]"
-    />
-    <van-field
-      v-model="password"
-      type="password"
-      name="password"
-      label="Password"
-      placeholder="Password"
-      :rules="[{ required: true, message: 'Password is required' }]"
-    />
-  </van-cell-group>
-  <div style="margin: 16px;">
-    <van-button round block type="primary" native-type="submit">
-      Submit
-    </van-button>
-  </div>
-</van-form>
-
-    </div>
-
-    <!-- <div style="margin-top: 20px;">
+    <div style="margin-top: 20px;">
       <van-form @submit="addTransaction">
         <van-field
           v-model="form.name"
@@ -72,7 +43,7 @@
         </van-swipe-cell>
         <van-loading v-if="loading" size="24" vertical>Loading...</van-loading>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
